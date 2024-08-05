be71bab3e9c1deb8fbc14190360a162b3bbe2994
MultiTron <iliev_evn@hotmail.com>
2024-08-05 14:38:45 +0300

    Added: Base & User Request/Response objects

---
+++ PCD.ApplicationServices/Messaging/StatusCode.cs
@@ -0,0 +1,11 @@
+﻿namespace PCD.ApplicationServices.Messaging
+{
+    public enum StatusCode
+    {
+        Success = 200,
+        Redirection = 300,
+        ClientError = 400,
+        NotFound = 404,
+        ServerError = 500
+    }
+}
