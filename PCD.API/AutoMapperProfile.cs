6549ca297445f5e4978736d448ea8b3789e31b58
MultiTron <iliev_evn@hotmail.com>
2024-08-05 16:40:32 +0300

    Added: Car AutoMapper profile

--- PCD.API/AutoMapperProfile.cs
+++ PCD.API/AutoMapperProfile.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using PCD.Data.Entities;
+using PCD.Infrastructure.DTOs.Cars;
 using PCD.Infrastructure.DTOs.Users;
 
 namespace PCD.API
@@ -21,7 +22,17 @@ namespace PCD.API
                 .ForMember(dest => dest.Id, opt => opt.MapFrom(x => x.Id))
                 .ForMember(dest => dest.FirstName, opt => opt.MapFrom(x => x.FirstName))
                 .ForMember(dest => dest.LastName, opt => opt.MapFrom(x => x.LastName));
-
+            CreateMap<CarAlterModel, Car>()
+                .ForMember(dest => dest.Id, opt => opt.Ignore())
+                .ForMember(dest => dest.Model, opt => opt.MapFrom(x => x.Model))
+                .ForMember(dest => dest.Brand, opt => opt.MapFrom(x => x.Brand))
+                .ForMember(dest => dest.Year, opt => opt.MapFrom(x => x.Year))
+                .ForMember(dest => dest.LicensePlateNumber, opt => opt.MapFrom(x => x.LicensePlateNumber))
+                .ForMember(dest => dest.CountryOfRegistration, opt => opt.MapFrom(x => x.CountryOfRegistration))
+                .ForMember(dest => dest.VIN, opt => opt.MapFrom(x => ""))
+                .ForMember(dest => dest.Trips, opt => opt.MapFrom(x => new List<Trip>()));
+            CreateMap<Car, UserViewModel>()
+                .ForAllMembers(opt => opt.MapFrom(x => x));
         }
     }
 }
