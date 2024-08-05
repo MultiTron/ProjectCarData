99386e8156e94fba1982d26ed46c1a6b0f6a994c
MultiTron <iliev_evn@hotmail.com>
2024-08-05 15:55:09 +0300

    Added: Get All Cars and Create Car services

--- PCD.ApplicationServices/Implementations/CarsManagementService.cs
+++ PCD.ApplicationServices/Implementations/CarsManagementService.cs
@@ -1,7 +1,12 @@
 ﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using PCD.ApplicationServices.Interfaces;
+using PCD.ApplicationServices.Messaging.Cars.Request;
+using PCD.ApplicationServices.Messaging.Cars.Response;
 using PCD.Data;
+using PCD.Data.Entities;
+using PCD.Infrastructure.DTOs.Cars;
 
 namespace PCD.ApplicationServices.Implementations
 {
@@ -11,6 +16,27 @@ namespace PCD.ApplicationServices.Implementations
         public CarsManagementService(ApplicationContext context, IMapper mapper, ILogger<CarsManagementService> logger) : base(mapper, logger)
         {
             _context = context;
+        }
+
+        public async Task<CreateCarResponse> CreateCar(CreateCarRequest request)
+        {
+            await _context.Cars.AddAsync(_mapper.Map<Car>(request.Car));
+            var status = await _context.SaveChangesAsync();
+            if (status > 0)
+            {
+                return new();
+            }
+            else
+            {
+                return new() { StatusCode = Messaging.StatusCode.ClientError };
+            }
+        }
+
+        public async Task<GetCarsResponse> GetAllCarsAsync()
+        {
+            var cars = new List<CarViewModel>();
+            await _context.Cars.ForEachAsync(x => cars.Add(_mapper.Map<CarViewModel>(x)));
+            return new(cars);
         }
     }
 }
