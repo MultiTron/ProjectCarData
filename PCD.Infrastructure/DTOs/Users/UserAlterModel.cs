e8f412b15a2f7a2bef2a2bd5f40d24a0e04ebb61
MultiTron <iliev_evn@hotmail.com>
2024-08-05 14:36:37 +0300

    Added: Infrastructure

---
+++ PCD.Infrastructure/DTOs/Users/UserAlterModel.cs
@@ -0,0 +1,8 @@
+﻿namespace PCD.Infrastructure.DTOs.Users
+{
+    public class UserAlterModel
+    {
+        required public string FirstName { get; set; }
+        required public string LastName { get; set; }
+    }
+}
