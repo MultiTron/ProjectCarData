2a8f769a9819b7a35486d5aa659ff32feff1149d
MultiTron <iliev_evn@hotmail.com>
2024-08-05 14:36:37 +0300

    Added: Infrastructure

---
+++ PCD.Infrastructure/DTOs/Cars/CarViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace PCD.Infrastructure.DTOs.Cars
+{
+    public class CarViewModel
+    {
+        required public int Id { get; set; }
+        required public string Brand { get; set; }
+        required public string Model { get; set; }
+        required public int Year { get; set; }
+        required public string LicensePlateNumber { get; set; }
+        required public string CountryOfRegistration { get; set; }
+    }
+}
---
+++ PCD.Infrastructure/DTOs/Users/UserViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace PCD.Infrastructure.DTOs.Users
+{
+    public class UserViewModel
+    {
+        required public int Id { get; set; }
+        required public string FirstName { get; set; }
+        required public string LastName { get; set; }
+    }
+}
