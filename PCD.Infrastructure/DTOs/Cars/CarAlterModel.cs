724ae335caa4c95f42fbaf70f5521fb459cc0c7a
MultiTron <iliev_evn@hotmail.com>
2024-08-05 15:54:05 +0300

    Added: Car object for Modification to Infrastructure

---
+++ PCD.Infrastructure/DTOs/Cars/CarAlterModel.cs
@@ -0,0 +1,11 @@
+﻿namespace PCD.Infrastructure.DTOs.Cars
+{
+    public class CarAlterModel
+    {
+        required public string Brand { get; set; }
+        required public string Model { get; set; }
+        required public int Year { get; set; }
+        required public string LicensePlateNumber { get; set; }
+        required public string CountryOfRegistration { get; set; }
+    }
+}
