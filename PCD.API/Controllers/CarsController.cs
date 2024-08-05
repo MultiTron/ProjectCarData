35ff5cc4ca0d47e1e975046247f54361a4a4ca6c
MultiTron <iliev_evn@hotmail.com>
2024-08-05 14:40:41 +0300

    Added: Controllers

---
+++ PCD.API/Controllers/CarsController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PCD.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CarsController : ControllerBase
+    {
+    }
+}
