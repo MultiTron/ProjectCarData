687061b8637933cee9a111de41bc11d02b7e3800
MultiTron <iliev_evn@hotmail.com>
2024-08-05 15:53:05 +0300

    Added: Car Request/Response objects

---
+++ PCD.ApplicationServices/Messaging/Cars/Response/GetCarsResponse.cs
@@ -0,0 +1,18 @@
+﻿using PCD.Infrastructure.DTOs.Cars;
+
+namespace PCD.ApplicationServices.Messaging.Cars.Response
+{
+    public class GetCarsResponse : BaseResponse
+    {
+        public List<CarViewModel> Cars { get; set; }
+
+        public GetCarsResponse(List<CarViewModel> cars)
+        {
+            Cars = cars;
+        }
+        public GetCarsResponse()
+        {
+            Cars = new List<CarViewModel>();
+        }
+    }
+}
