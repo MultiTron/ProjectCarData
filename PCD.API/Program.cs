f17e4ad009fc16bde40b4a96214f2c99dbd92213
MultiTron <iliev_evn@hotmail.com>
2024-08-05 14:48:05 +0300

    Revert "Added: Service Project and Infrastructure"
    This reverts commit 92c12f79e7f5406f47ffe96888aed0d041dd3c75.

--- PCD.API/Program.cs
+++ PCD.API/Program.cs
@@ -1,7 +1,4 @@
 using Microsoft.EntityFrameworkCore;
-using PCD.API;
-using PCD.ApplicationServices.Implementations;
-using PCD.ApplicationServices.Interfaces;
 using PCD.Data;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -10,13 +7,6 @@ var builder = WebApplication.CreateBuilder(args);
 var conn = builder.Configuration.GetConnectionString("DefaultConnectionString");
 builder.Services.AddDbContext<ApplicationContext>(options => options.UseSqlServer(conn, x => x.MigrationsAssembly("PCD.API")));
 
-builder.Services.AddScoped<IUsersManagementService, UsersManagementService>();
-builder.Services.AddScoped<ICarsManagementService, CarsManagementService>();
-
-builder.Logging.ClearProviders();
-builder.Logging.AddConsole();
-
-builder.Services.AddAutoMapper(typeof(AutoMapperProfile));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
