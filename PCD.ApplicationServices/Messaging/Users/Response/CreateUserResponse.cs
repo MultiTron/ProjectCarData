e66539440580a70860d68a48c839a3ff417b9dec
MultiTron <multitron03@duck.com>
2024-08-13 22:34:15 +0300

    Fix: Create user returns the user

--- PCD.ApplicationServices/Messaging/Users/Response/CreateUserResponse.cs
+++ PCD.ApplicationServices/Messaging/Users/Response/CreateUserResponse.cs
@@ -1,6 +1,17 @@
-﻿namespace PCD.ApplicationServices.Messaging.Users.Response;
+﻿using PCD.Infrastructure.DTOs.Users;
+
+namespace PCD.ApplicationServices.Messaging.Users.Response;
 
 public class CreateUserResponse : BaseResponse
 {
+    public UserViewModel? User { get; set; }
 
+    public CreateUserResponse(UserViewModel user)
+    {
+        User = user;
+    }
+    public CreateUserResponse()
+    {
+
+    }
 }
