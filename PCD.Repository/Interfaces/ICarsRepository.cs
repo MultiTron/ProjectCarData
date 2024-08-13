cd5916ce2aaa147d92e87c6acaab91d59da96aa4
MultiTron <multitron03@duck.com>
2024-08-13 14:53:10 +0300

    Added: Repository layer

---
+++ PCD.Repository/Interfaces/ICarsRepository.cs
@@ -0,0 +1,8 @@
+﻿using PCD.Data.Entities;
+
+namespace PCD.Repository.Interfaces
+{
+    public interface ICarsRepository : IRepository<Car>
+    {
+    }
+}
---
+++ PCD.Repository/Interfaces/IRepository.cs
@@ -0,0 +1,20 @@
+﻿using PCD.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PCD.Repository.Interfaces
+{
+    public interface IRepository<T> where T : BaseEntity
+    {
+        Task<IEnumerable<T>> GetAll();
+        Task<T> GetById(int id);
+        void Save(T entity);
+        void Insert(T entity);
+        void Update(T entity, string excludeProperties = "");
+        void Delete(T entity);
+        void Delete(int id);
+    }
+}
