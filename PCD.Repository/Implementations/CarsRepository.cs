a22819bb2825931a292efcea6cabd94641865bcb
MultiTron <multitron03@duck.com>
2024-08-13 14:53:10 +0300

    Added: Repository layer

---
+++ PCD.Repository/Implementations/CarsRepository.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PCD.Data.Entities;
+using PCD.Repository.Interfaces;
+
+namespace PCD.Repository.Implementations
+{
+    public class CarsRepository : Repository<Car>, ICarsRepository
+    {
+        public CarsRepository(DbContext context) : base(context)
+        {
+        }
+
+        public async override Task<IEnumerable<Car>> GetAll()
+            => await base.GetAll().Result.AsQueryable().Include("Trips").ToListAsync();
+    }
+}
