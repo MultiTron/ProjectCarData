426282b3e3cecbe582fd386699b36a0d3f4a896c
MultiTron <iliev_evn@hotmail.com>
2024-08-18 23:22:03 +0300

    Code Refactoring

--- PCD.API/Controllers/UsersController.cs
+++ PCD.API/Controllers/UsersController.cs
@@ -7,7 +7,7 @@ namespace PCD.API.Controllers;
 
 [Route("api/[controller]")]
 [ApiController]
-public class UsersController<T> : ControllerBase where T : BaseResponse
+public class UsersController : ControllerBase
 {
     private readonly IUsersManagementService _service;
 
@@ -20,63 +20,39 @@ public class UsersController<T> : ControllerBase where T : BaseResponse
     public async Task<IActionResult> Get()
     {
         var response = await _service.GetAllUsers();
-        if (response.StatusCode == ApplicationServices.Messaging.StatusCode.Success)
-        {
-            return Ok(response);
-        }
-        return BadRequest();
+        return Determin(response);
     }
     [HttpPost]
     public async Task<IActionResult> Create([FromBody] UserAlterModel model)
     {
         var response = await _service.CreateUser(new(model));
-        if (response.StatusCode == ApplicationServices.Messaging.StatusCode.Success)
-        {
-            return Ok(response);
-        }
-        return BadRequest();
+        return Determin(response);
     }
     [HttpDelete("User/{id}")]
     public async Task<IActionResult> Delete([FromRoute] int id)
     {
         var response = await _service.DeleteUser(new(id));
-        if (response.StatusCode == ApplicationServices.Messaging.StatusCode.Success)
-        {
-            return Ok(response);
-        }
-        return BadRequest();
+        return Determin(response);
     }
     [HttpGet("User/{id}")]
     public async Task<IActionResult> GetById([FromRoute] int id)
     {
         var response = await _service.GetUserById(new(id));
-        if (response.StatusCode == ApplicationServices.Messaging.StatusCode.Success)
-        {
-            return Ok(response);
-        }
-        return BadRequest();
+        return Determin(response);
     }
     [HttpGet("User/{id}/Cars")]
     public async Task<IActionResult> GetCarsByUser([FromRoute] int id)
     {
         var response = await _service.GetCarsByUser(new(id));
-        if (response.StatusCode == ApplicationServices.Messaging.StatusCode.Success)
-        {
-            return Ok(response);
-        }
-        return BadRequest();
+        return Determin(response);
     }
     [HttpPut("User/{id}")]
     public async Task<IActionResult> Update([FromRoute] int id, [FromBody] UserAlterModel model)
     {
         var response = await _service.UpdateUser(new(id, model));
-        if (response.StatusCode == ApplicationServices.Messaging.StatusCode.Success)
-        {
-            return Ok(response);
-        }
-        return BadRequest();
+        return Determin(response);
     }
-    private IActionResult Returner(T response)
+    private IActionResult Determin(BaseResponse response)
     {
         if (response.StatusCode == ApplicationServices.Messaging.StatusCode.Success)
         {
