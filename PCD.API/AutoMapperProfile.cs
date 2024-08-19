84cd3d59cc2de7a1d0c83b0191a7266aa83f2998
MultiTron <iliev_evn@hotmail.com>
2024-08-19 18:05:23 +0300

    Added: Trip Endpoints

--- PCD.API/AutoMapperProfile.cs
+++ PCD.API/AutoMapperProfile.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using PCD.Data.Entities;
 using PCD.Infrastructure.DTOs.Cars;
+using PCD.Infrastructure.DTOs.Trips;
 using PCD.Infrastructure.DTOs.Users;
 
 namespace PCD.API;
@@ -45,5 +46,18 @@ public class AutoMapperProfile : Profile
             .ForMember(dest => dest.CountryOfRegistration, opt => opt.MapFrom(x => x.CountryOfRegistration))
             .ForMember(dest => dest.VIN, opt => opt.MapFrom(x => x.VIN))
             .ForMember(dest => dest.UserId, opt => opt.MapFrom(x => x.UserId));
+        CreateMap<TripAlterModel, Trip>()
+            .ForMember(dest => dest.Id, opt => opt.Ignore())
+            .ForMember(dest => dest.Duration, opt => opt.MapFrom(x => x.Duration))
+            .ForMember(dest => dest.FuelConsumption, opt => opt.MapFrom(x => x.FuelConsumption))
+            .ForMember(dest => dest.Distance, opt => opt.MapFrom(x => x.Distance))
+            .ForMember(dest => dest.CarId, opt => opt.MapFrom(x => x.CarId))
+            .ForMember(dest => dest.CreatedOn, opt => opt.Ignore());
+        CreateMap<Trip, TripViewModel>()
+            .ForMember(dest => dest.Id, opt => opt.MapFrom(x => x.Id))
+            .ForMember(dest => dest.Duration, opt => opt.MapFrom(x => x.Duration))
+            .ForMember(dest => dest.FuelConsumption, opt => opt.MapFrom(x => x.FuelConsumption))
+            .ForMember(dest => dest.Distance, opt => opt.MapFrom(x => x.Distance))
+            .ForMember(dest => dest.CarId, opt => opt.MapFrom(x => x.CarId));
     }
 }
