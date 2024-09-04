758b43fd631b38d8ad273fb85292ec29bc4cdc20
MultiTron <iliev_evn@hotmail.com>
2024-09-04 16:55:39 +0300

    Added: Authorization to all Endpoints except the Login

--- PCD.API/Controllers/CustomControllerBase.cs
+++ PCD.API/Controllers/CustomControllerBase.cs
@@ -1,8 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using PCD.ApplicationServices.Messaging;
 
 namespace PCD.API.Controllers;
-
+[Authorize]
 public class CustomControllerBase : ControllerBase
 {
     protected IActionResult Output(BaseResponse response)
