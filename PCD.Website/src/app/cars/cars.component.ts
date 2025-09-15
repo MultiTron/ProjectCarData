a17a56a772d3efbfc85581331c6c1b9c0ea00850
Ivaylo Iliev <iliev_evn@hotmail.com>
2025-09-15 16:22:51 +0300

    Improved styling

--- PCD.Website/src/app/cars/cars.component.ts
+++ PCD.Website/src/app/cars/cars.component.ts
@@ -1,10 +1,11 @@
 import { Component, effect, inject } from '@angular/core';
 import { CarService } from './car.service';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-cars',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './cars.component.html',
   styleUrl: './cars.component.scss',
 })
