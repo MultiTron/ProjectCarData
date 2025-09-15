2d7ac2de99dd8a276bf16f04663693bd6ff6c885
Ivaylo Iliev <iliev_evn@hotmail.com>
2025-09-15 16:23:03 +0300

    Added PWA support

--- PCD.Website/src/app/app.config.ts
+++ PCD.Website/src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
@@ -8,10 +8,15 @@ import {
   withInterceptors,
 } from '@angular/common/http';
 import { authInterceptor } from './auth/auth.interceptor';
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideHttpClient(withFetch(), withInterceptors([authInterceptor])),
-  ],
+    provideServiceWorker('ngsw-worker.js', {
+        enabled: !isDevMode(),
+        registrationStrategy: 'registerWhenStable:30000'
+    })
+],
 };
