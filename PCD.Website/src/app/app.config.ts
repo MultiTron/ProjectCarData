35e424cefe160feaaf6ec40d0d743a36a5601d60
Ivaylo Iliev <iliev_evn@hotmail.com>
2025-09-18 09:26:59 +0300

    Update to angular v18

--- PCD.Website/src/app/app.config.ts
+++ PCD.Website/src/app/app.config.ts
@@ -2,11 +2,7 @@ import { ApplicationConfig, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import {
-  provideHttpClient,
-  withFetch,
-  withInterceptors,
-} from '@angular/common/http';
+import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 import { authInterceptor } from './auth/auth.interceptor';
 import { provideServiceWorker } from '@angular/service-worker';
 
